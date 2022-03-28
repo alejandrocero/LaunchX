8726e8728e105210b4ad140d1a82ac683942f9e1
Alejandro O <alejandro_cerp@hotmail.com>
2022-03-28 10:23:36 -0700

    pokedex

--- Frontend/Semana-4/pokedex.js
+++ Frontend/Semana-4/pokedex.js
@@ -1,12 +1,59 @@
 const fetchPokemon = () => {
-    const url = "https://pokeapi.co.api/v2/pokemon/chikorita";
-    fetch(url).then((res) => {
+  let pokeName = document.getElementById("pokeName").value.toLowerCase();
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+  fetch(url)
+    .then((res) => {
+      if (res.status != "200") {
+        alert("No existe ese pokemon.");
+      } else {
         return res.json();
-    }).then((data) => {
-        console.log(data);
-        let pokelog = data.sprites.front_default;
-        console.log(pokeImg)
+      }
     })
-}
-
-fetchPokemon();
+    .then((data) => {
+      if (data) {
+        console.log(data);
+        let pokeImg = data.sprites.front_default;
+        document.getElementById("name").innerHTML = data.name.toUpperCase();
+        document.getElementById("pokeImg").src = pokeImg;
+        if (data.types.length < 2) {
+          document.getElementById("type1").innerHTML =
+            data.types[0].type.name.toUpperCase();
+          document
+            .getElementById("type1")
+            .classList.replace(
+              document.getElementById("type1").classList.item(0),
+              data.types[0].type.name
+            );
+        } else {
+          document.getElementById("type1").innerHTML =
+            data.types[0].type.name.toUpperCase();
+          document
+            .getElementById("type1")
+            .classList.replace(
+              document.getElementById("type1").classList.item(0),
+              data.types[0].type.name
+            );
+          document.getElementById("type2").innerHTML =
+            data.types[1].type.name.toUpperCase();
+          document
+            .getElementById("type2")
+            .classList.replace(
+              document.getElementById("type2").classList.item(0),
+              data.types[1].type.name
+            );
+        }
+        document.getElementById("hp").innerHTML =
+          "HP: " + data.stats[0].base_stat;
+        document.getElementById("attack").innerHTML =
+          "ATTACK: " + data.stats[1].base_stat;
+        document.getElementById("defense").innerHTML =
+          "DEFENSE: " + data.stats[2].base_stat;
+        document.getElementById("sattack").innerHTML =
+          "S.ATTACK: " + data.stats[3].base_stat;
+        document.getElementById("sdefense").innerHTML =
+          "S.DEFENSE: " + data.stats[4].base_stat;
+        document.getElementById("speed").innerHTML =
+          "SPEED: " + data.stats[5].base_stat;
+      }
+    });
+};
